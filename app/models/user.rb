0ef3eb9ea37f582a7c9c84b907371d801e3cf85a
Andrea <andreaspagnolo@hotmail.com>
2013-02-28 11:19:05 +0100

    addemailindex

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,10 +14,9 @@ class User < ActiveRecord::Base
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :nom, :presence => true
+  validates :nom, :presence => true,
   				  :length => { :maximum => 20 }
-  validates :email, :presence => true
-  					:format => { :with => email_regex } 
+  validates :email, :presence => true,
+  					:format => { :with => email_regex }, 
   					:uniqueness => { :case_sensitive => false }
-
 end
