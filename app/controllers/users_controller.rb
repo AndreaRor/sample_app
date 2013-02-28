5d98536208355162c607e53dc693abb29f543d55
Andrea <andreaspagnolo@hotmail.com>
2013-02-28 16:01:22 +0100

    fin chap 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,25 @@
 class UsersController < ApplicationController
+  
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		flash[:success] = "Inscription reussie!"
+  		#flash[:success] = "Bienvenue dans l'Application Exemple!"
+  		redirect_to user_path(@user)
+  	else
+  		render 'new'
+  		#redirect_to signup_path
+  	end
+  end
+
   def new
   	@titre = "Inscription"
+  	@user = User.new
   end
 
   def show
   	@user = User.find(params[:id])
-  	#@titre = @user.nom
+  	@titre = @user.nom
   end
 
 end
