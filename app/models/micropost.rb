f6de2386c5f85d503b06cb4a2d763f3ece14870a
Andrea Spagnolo <andreaspagnolo@hotmail.com>
2013-03-07 10:07:09 +0100

    controller & modele commentaire

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -13,6 +13,8 @@ class Micropost < ActiveRecord::Base
   attr_accessible :content
 
   belongs_to :user
+  has_many :commentaires
+
   default_scope :order => "microposts.created_at DESC"
 
   validates :user_id, :presence => true
